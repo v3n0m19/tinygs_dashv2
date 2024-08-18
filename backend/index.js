8e009b9b65b462c7f5ea2be0b0dce83c886718fa
v3nom <rout.rudraprasad@gmail.com>
2024-08-18 16:41:18 +0530

    updated code for vercel deployment

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,19 @@ const connectDB = require('./config/db');
 const fetchAndStorePackets = require('./services/fetchAndStorePackets');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-connectDB();
 
 app.get('/fetch-packets', async (req, res) => {
+    await connectDB();
     const result = await fetchAndStorePackets();
     res.json(result);
 });
 
-setInterval(fetchAndStorePackets, 5 * 60 * 1000);
+if (process.env.NODE_ENV !== 'production') {
+    // Local development
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    });
+}
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app; // Export the app for Vercel
