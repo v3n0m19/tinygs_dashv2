a40593e403b51f6cfe107ebf5857f907d572f3e5
v3nom <rout.rudraprasad@gmail.com>
2024-08-19 11:05:37 +0530

    added root route

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,19 @@ const connectDB = require('./config/db');
 const fetchAndStorePackets = require('./services/fetchAndStorePackets');
 
 const app = express();
-
+app.get('/', (req,res) => res.send("tinyGS-dash API"))
 app.get('/fetch-packets', async (req, res) => {
     await connectDB();
     const result = await fetchAndStorePackets();
     res.json(result);
 });
 
-// if (process.env.NODE_ENV !== 'production') {
-//     // Local development
+if (process.env.NODE_ENV !== 'production') {
+    // Local development
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
         console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
     });
-// }
+}
 
 module.exports = app; // Export the app for Vercel
