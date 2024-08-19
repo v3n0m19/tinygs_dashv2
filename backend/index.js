047d6ed950fabc64724920f2ac2a5bd7fe981978
v3nom <rout.rudraprasad@gmail.com>
2024-08-19 11:02:46 +0530

    updated the code to make it listen on PORT/5000

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,12 @@ app.get('/fetch-packets', async (req, res) => {
     res.json(result);
 });
 
-if (process.env.NODE_ENV !== 'production') {
-    // Local development
+// if (process.env.NODE_ENV !== 'production') {
+//     // Local development
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
         console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
     });
-}
+// }
 
 module.exports = app; // Export the app for Vercel
